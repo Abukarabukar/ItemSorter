f1a7772ef41139ffd448aa8074ee1c30388f8c2b
Abukarabukare <Abukarshegow@gmail.comm>
2024-04-10 16:52:06 -0400

    finished

--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,26 @@
 package rocks.zipcode.io.comparators;
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+import java.util.Objects;
 
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item>  {
+    @Override
+    public int compare(Item item, Item t1) {
+
+
+        if (item.getPrice() > t1.getPrice()) {
+            return 1;
+        } else if (Objects.equals(item.getPrice(),t1.getPrice() )){
+            return 0;
+        }
+
+return -1;
+
+
+    }
+
 }
